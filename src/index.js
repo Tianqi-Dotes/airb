0dcb42c8a882d8b6a407a3ce94c3cba0ba72d5ad
Tq <tianqitobethefirst@gmail.com>
2023-10-10 16:54:35 +0800

    init store

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import App from './App';
 import './assets/css/index.less'
 import "normalize.css"
 import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <Suspense fallback="loading">
+      <Provider store={store}>
       <HashRouter>
         <App />
       </HashRouter>
+      </Provider>
     </Suspense>
 
 );
